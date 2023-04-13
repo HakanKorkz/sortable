43ad0d6afc44ddac2cdabf519e559cd1637455c3
hakan <hakan.hizmet.54.gs@gmail.com>
2023-04-14 02:35:13 +0300

    Cross control işlemi fixlendi

--- server/App/config/CorsHandler.php
+++ server/App/config/CorsHandler.php
@@ -28,60 +28,48 @@ class CorsHandler
      */
     public function handleCors(): void
     {
-        if ($_SERVER['REQUEST_METHOD'] !== 'OPTIONS') {
-            if (empty($_SERVER['HTTP_ORIGIN'])) {
-                return;
+        if (!empty($_SERVER['HTTP_ORIGIN'])) {
+            $requestOrigin = filter_var(trim($_SERVER['HTTP_ORIGIN']), FILTER_VALIDATE_URL);
+            $referer = filter_var(trim($_SERVER['HTTP_REFERER']), FILTER_VALIDATE_URL);
+            if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+                header('Access-Control-Allow-Origin: ' . "$requestOrigin");
+                header('Access-Control-Allow-Headers: Content-Type');
+                header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+                exit;
+            }
+            if ($this->isOriginAllowed($requestOrigin, $referer)) {
+                header('Access-Control-Allow-Origin: ' . $requestOrigin);
+                header('Access-Control-Allow-Headers: Content-Type');
+                header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+            } else {
+                http_response_code(403);
+                header('Content-Type: text/plain; charset=utf-8');
+                echo 'Yasaklı erişim isteğinde bulundunuz.';
+                exit;
             }
         }
-        $requestOrigin = filter_var(trim($_SERVER['HTTP_ORIGIN'] ?? ''), FILTER_VALIDATE_URL);
-        $referer = filter_var(trim($_SERVER['HTTP_REFERER'] ?? ''), FILTER_VALIDATE_URL);
-
-        if ($this->isOriginAllowed($requestOrigin, $referer)) {
-            header('Access-Control-Allow-Origin: ' . $requestOrigin);
-            header('Access-Control-Allow-Headers: Content-Type');
-            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE,OPTIONS');
-        } else {
-            http_response_code(403);
-            header('Content-Type: text/plain; charset=utf-8');
-            echo 'Yasaklı erişim isteğinde bulundunuz.';
-            exit;
-        }
+        return;
     }
 
-//    public function handleCors(): void
-//    {
-//        $requestOrigin = filter_var(trim($_SERVER['HTTP_ORIGIN'] ?? ''), FILTER_VALIDATE_URL);
-//        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-//            if (empty($_SERVER['HTTP_ORIGIN'])) {
-//                return;
-//            }
-//            header('Access-Control-Allow-Origin: '.$requestOrigin);
-//            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-//            header('Access-Control-Allow-Headers: Content-Type');
-//            exit;
-//        }
-//        $referer = filter_var(trim($_SERVER['HTTP_REFERER'] ?? ''), FILTER_VALIDATE_URL);
-//
-//        if ($this->isOriginAllowed($requestOrigin, $referer)) {
-//            header('Access-Control-Allow-Origin: ' . $requestOrigin);
-//            header('Access-Control-Allow-Headers: Content-Type');
-//            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE,OPTIONS');
-//        } else {
-//            http_response_code(403);
-//            header('Content-Type: text/plain; charset=utf-8');
-//            echo 'Yasaklı erişim isteğinde bulundunuz.';
-//            exit;
-//        }
-//    }
-
-    private function isOriginAllowed(string $requestOrigin, string $referer): bool {
-        foreach ($this->allowedOrigins as $allowedOriginsList) {
-            foreach ($allowedOriginsList as $allowedOrigin) {
-                if (str_starts_with($requestOrigin, $allowedOrigin) && str_starts_with($referer, $allowedOrigin)) {
-                    return true;
-                }
-            }
+    /**
+     * @param string $requestOrigin
+     * @param string $referer
+     * @return bool
+     */
+    private function isOriginAllowed(string $requestOrigin, string $referer): bool
+    {
+         $allowedOrigins=$this->allowedOrigins["allowedOrigins"];
+        if (in_array("$requestOrigin/",$allowedOrigins) && in_array($referer,$allowedOrigins)) {
+            return true;
         }
+
+//        foreach ($this->allowedOrigins as $allowedOriginsList) {
+//            foreach ($allowedOriginsList as $allowedOrigin) {
+//                if (str_starts_with($requestOrigin, $allowedOrigin) && str_starts_with($referer, $allowedOrigin)) {
+//                    return true;
+//                }
+//            }
+//        }
         return false;
     }
 }
